f99b99f04b0c7dcf414a2e1f8ac61c25cdf0ae38
ono shinji <yjsnpi555@gmail.com>
2020-05-14 15:16:54 +0900

    ヴァリデーションの設定

--- app/controllers/tweet_clones_controller.rb
+++ app/controllers/tweet_clones_controller.rb
@@ -2,16 +2,24 @@ class TweetClonesController < ApplicationController
   def index
     @tweetposts = TweetClone.all
   end
+
   def new
     @tweet_clone = TweetClone.new
   end
+
   def create
-    TweetClone.create(tweet_params)
-    redirect_to new_tweet_clone_path
+    @tweet_clone = TweetClone.new(tweet_params)
+    if @tweet_clone.save
+    redirect_to tweet_clones_path, notice: "投稿を作成しました"
+    else
+      render :new
+    end
   end
+
   def show
     @tweet_clone = TweetClone.find(params[:id])
   end
+  
   private
   def tweet_params
     params.require(:tweet_clone).permit(:content) #ストロングパラメータにし、さらにprivateメソッド配下にする
