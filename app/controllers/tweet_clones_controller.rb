8907c23963cc3644712b2b2aba1f294af9eb92fa
ono shinji <yjsnpi555@gmail.com>
2020-05-14 14:19:50 +0900

    投稿機能を作成

--- app/controllers/tweet_clones_controller.rb
+++ app/controllers/tweet_clones_controller.rb
@@ -2,5 +2,14 @@ class TweetClonesController < ApplicationController
   def index
   end
   def new
+    @tweet_clone = TweetClone.new
+  end
+  def create
+    TweetClone.create(tweet_params)
+    redirect_to new_tweet_clone_path
+  end
+  private
+  def tweet_params
+    params.require(:tweet_clone).permit(:content) #ストロングパラメータにし、さらにprivateメソッド配下にする
   end
 end
